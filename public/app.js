c909105618c8cc1fed5a01241b3baabf33cbb7f4
garmatyair-design <garmat.yair@gmail.com>
2025-12-30 12:53:51 -0600

    Refactor data fetching and rendering functions

--- public/app.js
+++ public/app.js
@@ -1,27 +1,19 @@
-const tabla = document.getElementById("tabla");
-
-const map = L.map("mapa").setView([23.6, -102.5], 5);
-L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png").addTo(map);
-
+let inversiones = [];
 let markers = [];
 
-async function cargarDatos() {
-  const empresa = document.getElementById("buscarEmpresa").value;
-  const tipo = document.getElementById("tipoInversion").value;
+async function buscarInversiones() {
+  const res = await fetch("/api/fuentesMasivas");
+  inversiones = await res.json();
+  renderTabla();
+  renderMapa();
+}
 
-  const q = new URLSearchParams();
-  if (empresa) q.append("empresa", empresa);
-  if (tipo) q.append("tipo", tipo);
+function renderTabla() {
+  const tbody = document.getElementById("tabla");
+  tbody.innerHTML = "";
 
-  const r = await fetch("/api/inversiones?" + q);
-  const data = await r.json();
-
-  tabla.innerHTML = "";
-  markers.forEach(m => map.removeLayer(m));
-  markers = [];
-
-  data.forEach(i => {
-    tabla.innerHTML += `
+  inversiones.forEach(i => {
+    tbody.innerHTML += `
       <tr>
         <td>${i.empresa}</td>
         <td>${i.tipo_inversion}</td>
@@ -29,20 +21,19 @@ async function cargarDatos() {
         <td>${i.anio_inicio}</td>
         <td><a href="${i.url}" target="_blank">Fuente</a></td>
       </tr>`;
-
-    const m = L.marker([i.lat, i.lng])
-      .addTo(map)
-      .bindPopup(`<b>${i.empresa}</b><br>${i.tipo_inversion}`);
-    markers.push(m);
   });
 }
 
-document.getElementById("buscarTop").onclick = async () => {
-  await fetch("/api/rssFetcher");
-  cargarDatos();
-};
+function renderMapa() {
+  markers.forEach(m => map.removeLayer(m));
+  markers = [];
 
-document.getElementById("buscarEmpresa").oninput = cargarDatos;
-document.getElementById("tipoInversion").onchange = cargarDatos;
+  inversiones.forEach(i => {
+    const marker = L.marker([i.lat, i.lng])
+      .addTo(map)
+      .bindPopup(`<b>${i.empresa}</b><br>${i.tipo_inversion}`);
+    markers.push(marker);
+  });
+}
 
-cargarDatos();
+document.getElementById("buscarTop").addEventListener("click", buscarInversiones);
